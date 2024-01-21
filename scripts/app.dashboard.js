0d9a02b540fb509a633dde4334f623deda92915b
dewacahaya <dewacahaya30@gmail.com>
2024-01-21 13:13:37 +0800

    list jokes

--- scripts/app.dashboard.js
+++ scripts/app.dashboard.js
@@ -1,8 +1,31 @@
 // == TEMPLATE QUIZ ITEM ==
 
-// generateQuizItem({
-//   id: question.id,
-//   question: question.jokes,
-//   answer: question.answer,
-//   category: question.category,
-// });
+import { getQuestions, createQuestion } from "./api.js";
+import { generateQuizItem } from "./utils/index.js";
+
+const quizContent = document.getElementById("quiz-content");
+
+document.addEventListener("DOMContentLoaded", () => {
+  async function handleAllQuestion() {
+    try {
+      const questions = await getQuestions();
+
+      if (!questions) return;
+      quizContent.innerHTML = questions.map((question) => {
+        return generateQuizItem({
+            id: question.id,
+            question: question.jokes,
+            answer: question.answer,
+            category: question.category,
+          });
+      }).join("")
+
+      console.log({ questions });
+    } catch (error) {
+      console.error("Error", {
+        error,
+      });
+    }
+  }
+  handleAllQuestion();
+});
